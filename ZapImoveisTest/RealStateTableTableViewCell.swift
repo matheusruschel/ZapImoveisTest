94e8fb657e6d5a8a90276ca12ba96ec54041cb3b
Matheus Ruschel <matheus.ruschel@gmail.com>
2016-08-18 23:54:40 -0300

    added initial view setup

--- ZapImoveisTest/RealStateTableTableViewCell.swift
+++ ZapImoveisTest/RealStateTableTableViewCell.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class RealStateTableTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var labelAVenda: UILabel!
+    @IBOutlet weak var labelAVendaPrice: UILabel!
+    @IBOutlet weak var labelLocacao: UILabel!
+    @IBOutlet weak var labelLocacaoPrice: UILabel!
+    
+    @IBOutlet weak var buttonEye: UIButton!
+    @IBOutlet weak var buttonTrashCan: UIButton!
+    
+    @IBOutlet weak var imageViewRealState: UIImageView!
+    @IBOutlet weak var labelNumber: UILabel!
+    @IBOutlet weak var labelName: UILabel!
+    
+    @IBOutlet weak var labelStreet: UILabel!
+    
+    @IBOutlet weak var imageViewIconMessage: UIImageView!
+    @IBOutlet weak var imageViewIconExpirationDate: UIImageView!
+    
+    @IBOutlet weak var labelMsgs: UILabel!
+    
+    @IBOutlet weak var labelExpirationDate: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- ZapImoveisTest/RealStateTableViewController.swift
+++ ZapImoveisTest/RealStateTableViewController.swift
@@ -8,12 +8,93 @@
 
 import UIKit
 
-class RealStateTableViewController: UIViewController {
+let nibIdentifier = "RealStateTableTableViewCell"
+let cellIdentifier = "RealStateCell"
 
+class RealStateTableViewController: UIViewController {
+    
+    @IBOutlet weak var realStateTableView: UITableView! {
+        didSet {
+            configureTableView()
+        }
+    }
+    
+    @IBOutlet weak var toolBar: UIToolbar! {
+        didSet {
+            configureToolBar()
+        }
+    }
+    
+    @IBOutlet weak var buttonMagnifier: UIBarButtonItem!
+    @IBOutlet weak var buttonCone: UIBarButtonItem!
+    @IBOutlet weak var segmenetedControl: UISegmentedControl!
+    @IBOutlet weak var lowerToolBar: UIToolbar! {
+        didSet {
+            configureLowerToolBar()
+        }
+    }
+    
+    @IBOutlet weak var labelAtualizadoAInstantes: UILabel! {
+        didSet {
+            configureLabelAtualizadoAInstantes()
+        }
+    }
+    @IBOutlet weak var labelNumberOfAnuncios: UILabel! {
+        didSet {
+            configureLabelNumberOfAnuncios()
+        }
+    }
+    
+    @IBOutlet weak var buttonOrdenar: UIBarButtonItem!
+    @IBOutlet weak var buttonOrdenarMenu: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.appBlueColor()
+        configureNavigationBar()
+    }
+    
+    // MARK: CONFIGURATIONS
+    
+    func configureLabelAtualizadoAInstantes() {
+        labelAtualizadoAInstantes.textColor = UIColor.whiteColor()
+    }
+    
+    func configureLabelNumberOfAnuncios() {
+        labelNumberOfAnuncios.textColor = UIColor.whiteColor()
+    }
+    
+    func configureTableView() {
+        self.realStateTableView.delegate = self
+        self.realStateTableView.dataSource = self
+        let cellNib = UINib(nibName: nibIdentifier, bundle: nil)
+        self.realStateTableView.registerNib(cellNib, forCellReuseIdentifier: cellIdentifier)
+    }
+    
+    func configureToolBar() {
+        self.toolBar.delegate = self
+        self.toolBar.setBackgroundImage(UIImage(), forToolbarPosition: .TopAttached, barMetrics: .Default)
+        self.toolBar.backgroundColor = UIColor.appBlueColor()
+    }
+    
+    func configureLowerToolBar() {
+        self.lowerToolBar.delegate = self
+        self.lowerToolBar.setBackgroundImage(UIImage(), forToolbarPosition: .Bottom, barMetrics: .Default)
+        self.lowerToolBar.alpha = 0.9
+        self.lowerToolBar.backgroundColor = UIColor.appBlueColor()
+        self.lowerToolBar.tintColor = UIColor.whiteColor()
+    }
+    
+    func configureNavigationBar() {
+        
+        let navigationBar = self.navigationController!.navigationBar
+        
+        navigationBar.setBackgroundImage(UIImage(), forBarPosition: .Any, barMetrics: .Default)
+        navigationBar.backgroundColor = UIColor.appBlueColor()
+        navigationBar.shadowImage = UIImage()
+        navigationBar.tintColor = UIColor.whiteColor()
+        navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,4 +113,39 @@ class RealStateTableViewController: UIViewController {
     }
     */
 
+}
+
+// MARK:  TABLE VIEW DELEGATE & DATA SOURCE
+
+extension RealStateTableViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! RealStateTableTableViewCell
+        
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 200
+    }
+    
+}
+// MARK:  TOOL BAR DELEGATE
+extension RealStateTableViewController : UIToolbarDelegate {
+    func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
+        
+        if bar === self.toolBar {
+            return .TopAttached
+        } else {
+            return .Bottom
+        }
+        
+    }
 }
